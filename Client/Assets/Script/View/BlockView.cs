4a9cf94f38c9e63048dcc61fe251a57e18713ddf
Seung Ho Jang <shjang1992@gmail.com>
2016-07-19 19:00:16 +0900

    Unused code removed

--- Client/Assets/Script/View/BlockView.cs
+++ Client/Assets/Script/View/BlockView.cs
@@ -20,15 +20,4 @@ public class BlockView : MonoBehaviour {
         transform.position = new Vector2(block.X, block.Y);
         text.text = block.Value.ToString();
     }
-
-    public static Vector3 GetScreenPosition(Transform transform, Canvas canvas, Camera cam)
-    {
-        Vector3 pos;
-        float width = canvas.GetComponent<RectTransform>().sizeDelta.x;
-        float height = canvas.GetComponent<RectTransform>().sizeDelta.y;
-        float x = Camera.main.WorldToScreenPoint(transform.position).x / Screen.width;
-        float y = Camera.main.WorldToScreenPoint(transform.position).y / Screen.height;
-        pos = new Vector3(width * x - width / 2, y * height - height / 2);
-        return pos;
-    }
 }
