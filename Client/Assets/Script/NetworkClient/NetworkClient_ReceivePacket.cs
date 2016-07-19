0a56607a273de6f0dd7b85116209b0c54bb7c644
Seung Ho Jang <shjang1992@gmail.com>
2016-07-20 02:21:12 +0900

    Ranking debug output added

--- Client/Assets/Script/NetworkClient/NetworkClient_ReceivePacket.cs
+++ Client/Assets/Script/NetworkClient/NetworkClient_ReceivePacket.cs
@@ -3,5 +3,11 @@
 public partial class NetworkClient {
     private void OnPacketReceive(PKS.SC_HIGH_SCORE_LIST pks)
     {
+        for(int i = 0; i < pks.name.Count; ++i) {
+            var name = pks.name[i];
+            var score = pks.score[i];
+
+            Debug.Log("name : " + name + ", score : " + score);
+        }
     }
 }
